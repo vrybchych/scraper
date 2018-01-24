33ec364a407051fe882adb5e7fc86c5915525274
vrybchych <vitaliy.rybchych@gmail.com>
2018-01-24 13:23:20 +0000

    modify WGET add -D option

--- WGET.sh
+++ WGET.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 URL=$1
-wget -r -nd -P $URL --max-redirect 0 -R avi,bmp,css,djvu,doc,docx,gif,GIF,gz,ico,jpeg,jpg,JPG,js,json,mp3,mp4,ogv,pdf,png,ppt,ps,rar,rss,svg,swf,tex,tmp,txt,txt.tmp,webm,xls,xlsx,xml,zip $URL
+wget -r -nd -P $URL -D $URL -R avi,bmp,css,djvu,doc,docx,gif,GIF,gz,ico,jpeg,jpg,JPG,js,json,mp3,mp4,ogv,pdf,png,ppt,ps,rar,rss,svg,swf,tex,tmp,txt,txt.tmp,webm,xls,xlsx,xml,zip $URL
